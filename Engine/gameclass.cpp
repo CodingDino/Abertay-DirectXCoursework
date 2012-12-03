02d4fdea3c1c44268d339114c27c1cec349f5774
Sarah Herzog <sarah.ann.herzog@gmail.com>
2012-12-03 18:31:09 +0000

    Add framework created in tutorial

---
+++ Engine/gameclass.cpp
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: gameclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "gameclass.h"
+
+
+GameClass::GameClass()
+{
+}
+
+
+GameClass::GameClass(const GameClass& other)
+{
+}
+
+
+GameClass::~GameClass()
+{
+}
+
+
+bool GameClass::Initialize()
+{
+	return true;
+}
+
+
+void GameClass::Shutdown()
+{
+	return;
+}
+
+
+bool GameClass::Frame()
+{
+	return true;
+}
---
+++ Engine/graphicsclass.h
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "d3dclass.h"
+#include "cameraclass.h"
+#include "modelclass.h"
+#include "lightshaderclass.h"
+#include "lightclass.h"
+
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = true;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+const float NUM_MODELS = 2;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: GraphicsClass
+////////////////////////////////////////////////////////////////////////////////
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render(float, float);
+
+private:
+	D3DClass* m_D3D;
+	CameraClass* m_Camera;
+	LightShaderClass* m_LightShader;
+	LightClass* m_Light;
+
+	// Models
+	ModelClass* m_Model;
+	ModelClass* m_Model_2;
+//	ModelClass*[NUM_MODELS] m_Models;
+};
+
+#endif
